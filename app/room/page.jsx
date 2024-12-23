24a00799f38909f4b9093e2b108b13cfabd92981
keshavsaini0311 <keshavsaini1234567890@gmail.com>
2024-12-24 02:03:15 +0530

    video uploaded to firebase but not able to save to user something wrong with fetch vid

--- app/room/page.jsx
+++ app/room/page.jsx
@@ -1,88 +1,117 @@
 "use client"
-import DropFileInput from '@/components/drop-file-input/DropFileInput';
-import UploadButton from '@/components/upload-button/uploadButton';
-import { useState } from 'react';
-import { storage } from '@/lib/firebase';
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { useState,useEffect,useRef } from 'react';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { app } from '@/lib/firebase'
+import img from '@/public/cloud-upload-regular-240.png'
+import { useRouter } from 'next/navigation';
+
 function page() {
-// Create the file metadata
-/** @type {any} */
-const metadata = {
-  contentType: 'video/mp4'
-};
 
-// Upload file and metadata to the object 'images/mountains.jpg'
-const storageRef = ref(storage, 'videos/' + file);
-const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+  const router=useRouter();
+  const fileRef = useRef()
+  const[file, setFile] = useState(undefined)
+  const [filePerc, setFilePerc] = useState(0)
+  const [fileUploadError, setFileUploadError] = useState(false)
+  const[vidurl,setVidurl]=useState("")
+  const [id, setId] = useState('');
 
-// Listen for state changes, errors, and completion of the upload.
-
-
-
-    const [file, setFile] = useState(null)
-
-    const onFileChange = (files) => {
-        const currentFile = files[0]
-        setFile(currentFile)
-        console.log(files);
+  useEffect(() => {
+    const getuserid = async () => {
+      const response = await fetch('/api/me');
+      const data = await response.json();
+      console.log(data);
+      
+      setId(data._id);
     }
+    getuserid();
+  }, [setId]);
 
-
-    const handleClick = () => {
-        if(file === null) {
-            return
-        }
-        uploadTask.on('state_changed',
-            (snapshot) => {
-              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-              const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-              console.log('Upload is ' + progress + '% done');
-              switch (snapshot.state) {
-                case 'paused':
-                  console.log('Upload is paused');
-                  break;
-                case 'running':
-                  console.log('Upload is running');
-                  break;
-              }
-            }, 
-            (error) => {
-              // A full list of error codes is available at
-              // https://firebase.google.com/docs/storage/web/handle-errors
-              switch (error.code) {
-                case 'storage/unauthorized':
-                  // User doesn't have permission to access the object
-                  break;
-                case 'storage/canceled':
-                  // User canceled the upload
-                  break;
-          
-                // ...
-          
-                case 'storage/unknown':
-                  // Unknown error occurred, inspect error.serverResponse
-                  break;
-              }
-            }, 
-            () => {
-              // Upload completed successfully, now we can get the download URL
-              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                console.log('File available at', downloadURL);
-              });
-            }
-          );
+  useEffect(() => {
+    if(file){
+      handleFileUpload(file)
     }
+  }, [file])
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app)
+    const fileName = file.name
+    const storageRef = ref(storage, fileName)
+    const uploadTask = uploadBytesResumable(storageRef, file)
 
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        setFilePerc(Math.round(progress))
+        },
+      (error) => {
+        setFileUploadError(true)
+        },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+         
+           setVidurl(downloadURL);
+        })
+      }
+    )
+  }
+
+
+  const handlesubmit = async () => {
+    try {
+      if(!vidurl||!id){
+        console.log("data not present");
+        return;
+      }
+      const jsondata={
+        vidurl:vidurl,
+        id:id
+      }
+      console.log(jsondata);
+      
+      const res = await fetch("/api/vid", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jsondata),
+      });
+      const data = await res.json();
+      console.log(data);
+      
+      if(data.success===false){
+        toast.error(data.message);
+        return
+      }
+      //router.push(`/room/${id}`);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log(vidurl);
+  console.log(id);
+  
     return (
         <div className="box">
-            <h2 className="header">
-                React drop files input
-            </h2>
-            <DropFileInput
-                onFileChange={(files) => onFileChange(files)}
-            />
-            <br></br>
-            <UploadButton onClick={ () => handleClick()}> </UploadButton>
+            
+            <form  className='flex flex-col gap-4'>
+        <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} className="hidden" accept="video/*" />
+        <img onClick={() => fileRef.current.click()} className='mt-2 self-center cursor-pointer object-cover w-32 h-32 rounded-full' src={img} alt="" />
+<p className='text-center'>
+  {fileUploadError ? (
+    <span className='text-red-500'>Upload Failed</span>
+  ): filePerc >0&& filePerc < 100 ? (
+      <span className='t ext-slate-500'>{filePerc}%</span>
+    ): filePerc === 100 ? (
+        <span className='text-green-500'>Uploaded Successfully</span>
+      ): (
+        ""
+      )}
+      </p>
+      <button onClick={handlesubmit}>go to room</button>
+      </form>
         </div>
     );
 }
